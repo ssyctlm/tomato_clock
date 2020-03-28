70530aec40d237de3bae615965e92f4c70f336da
SmartDell <myqy480@gmail.com>
2020-03-28 16:58:00 +0800

    second change

--- src/components/tomatoclock.js
+++ src/components/tomatoclock.js
@@ -6,37 +6,101 @@ export default class TomatoClock extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      val: 1,
-      break: 5,
-      section: 5,
+      break: ['break', 5],
+      session: ['session', 25],
+      pass_time: [],
+      timer: '25:00'
 
     }
 
+  }
+  updateTimer = (time) => {
+    let innerTime = time * 60;
+    let min = Math.floor(innerTime / 60);
+    let second = innerTime % 60;
+    let secondShow = second === 0 ? '00' : toString(second);
+    this.setState({ timer: `${min}:${secondShow}` })
+  }
+
+  handlebreakAdd = () => {
+    if (this.state.break[1] < 59) {
+      let temp = this.state.break[1] += 1;
+      this.setState(
+        { break: ['break', temp] }
+      )
+    }
+  }
+  handlebreakSub = () => {
+    if (this.state.break[1] > 0) {
+      this.setState({ break: ['break', (this.state.break[1] -= 1)] })
+
+    }
+
+  }
+  handlesessionAdd = () => {
+    if (this.state.session[1] < 59) {
+      this.setState({ session: ['session', (this.state.session[1] += 1)] });
+      this.updateTimer(this.state.session[1]);
+    }
+  }
+  handlesessionSub = () => {
+    if (this.state.session[1] > 0) {
+      this.setState({ session: ['session', (this.state.session[1] -= 1)] });
+      this.updateTimer(this.state.session[1]);
+    }
+  }
+
+  countDown = () => {
+    let time = this.state.session[1];
+    console.log(time);
+    setInterval(this.updateTimer(time), 1000);
+    time--;
+    console.log(time);
+    this.updateTimer(time);
+    console.log(time);
 
   }
   render() {
     return (
       <div className="wrapper">
         <h1>Pomodoro Clock</h1>
-        <div className="adjust-box">
-          <h4 id="break-label">Break Length</h4>
-          <p>{`+ ${this.state.break} -`}</p>
-        </div>
-        <div className="adjust-box">
-          <h4 id="session-label">Session Length</h4>
-          <p>{`+ ${this.state.section} -`}</p>
-        </div>
-        <div className="display-panel">
-          <div className="switchs">
-            <span>></span>
-            <span>||</span>
-            <span>O</span>
+        <div className="gridbox-outer">
+          <div className="adjust-box">
+            <h4 id="break-label">Break Length</h4>
+            <div className="controller">
+              <input className="btn" type="button" value="+" onClick={this.handlebreakAdd} />
+              <p>{this.state.break[1]}</p>
+              <input className="btn" type="button" value="-" onClick={this.handlebreakSub} />
+            </div>
+
           </div>
-          <div className="dail">
-            <h4>Sesscion</h4>
-            <p>25:00</p>
+          <div className="adjust-box">
+            <h4 id="session-label">Session Length</h4>
+            <div className="controller">
+              <input className="btn" type="button" value="+"
+                onClick={this.handlesessionAdd} />
+              <p>{this.state.session[1]}</p>
+              <input className="btn" type="button" value="-"
+                onClick={this.handlesessionSub} />
+            </div>
           </div>
+          <div className="display-panel">
+            <div className="switchs">
+              <button className="btn" title="start" onClick={this.countDown}>></button>
+              <button className="btn" title="pause">||</button>
+              <button className="btn" title="restart">O</button>
+            </div>
+            <div className="dail">
+              <h4>{this.state.session[0]}</h4>
+              <p>{this.state.timer}</p>
+            </div>
+          </div>
+
+
         </div>
+
+
+
       </div>
 
     )
